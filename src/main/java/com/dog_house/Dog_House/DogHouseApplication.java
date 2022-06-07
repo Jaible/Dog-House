ca4ffdf8460995927d1d4bad2aff61f1b45f4105
Jaible <jaiblemd@gmail.com>
2022-06-06 22:42:51 -0600

    Adición del Index

---
+++ src/main/java/com/dog_house/Dog_House/DogHouseApplication.java
@@ -0,0 +1,13 @@
+package com.dog_house.Dog_House;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dog_house/Dog_House/DogHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dog_house.Dog_House;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
